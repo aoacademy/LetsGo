3cb190b10cc9657c04e663e469d6358c2e13625d
Parham Alvani <parham.alvani@gmail.com>
2019-05-11 21:19:06 +0430

    Corrects main.go

--- main.go
+++ main.go
@@ -2,31 +2,35 @@ package main
 
 import (
 	"context"
-	"time"
 	"fmt"
 	"net/http"
-	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type test struct {
-  lat  string `json:"name" form:"name" query:"name"`
-  lng string `json:"email" form:"email" query:"email"`
+	lat string `json:"lat"`
+	lng string `json:"lng"`
 }
 
-func JsonHandler(c echo.Context) (err error) {
-  t := new(test)
-  if err = c.Bind(t); err == nil{
-    return err
-  }
-  client, err := mongo.NewClient(options.Client().ApplyURI("localhost:27017"))
-  ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-  err = client.Connect(ctx)
-  collection := client.Database("testing").Collection("lat_lng")
-  res, err := collection.InsertOne(ctx,t)
-  id := res.InsertedID
-  return c.String(http.StatusOK, fmt.Sprintf("man amadam with id: %s", id))
+// JSONHandler handles request json.
+func JSONHandler(c echo.Context) (err error) {
+	t := new(test)
+	if err = c.Bind(t); err == nil {
+		return err
+	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	collection := client.Database("testing").Collection("lat_lng")
+	res, err := collection.InsertOne(ctx, t)
+	id := res.InsertedID
+	return c.String(http.StatusOK, fmt.Sprintf("man amadam with id: %s", id))
 }
 
 func main() {
@@ -34,6 +38,6 @@ func main() {
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "")
 	})
-	e.POST("/posting",JsonHandler)
+	e.POST("/posting", JSONHandler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
